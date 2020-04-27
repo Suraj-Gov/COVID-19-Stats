8a443a8a48ce4b33659d69a160bee34edba45fe4
Suraj Govind <surajgovind332000@gmail.com>
2020-04-27 21:06:36 +0530

    Adjusted main page, added horizontal scrolling!

--- src/index.js
+++ src/index.js
@@ -7,8 +7,14 @@ const Main = () => {
   return (
     <div className="main-container">
       <h1 className="main-heading">COVID-19 Stats</h1>
+      <h4 className="main-subtitle">A COVID-19 stats webapp that's updated automatically.</h4>
       <div id="country-container">
         <TopCountries />
+      </div>
+      <h4 className="scroll-subtitle">🢘 countries with highest impact 🢚</h4>
+      <div className="footer">
+        <h4 className="main-subtitle">Created with ❤ | Created with React & GitHub</h4>
+        <h4 className="main-subtitle">More features coming soon...</h4>
       </div>
     </div>
   )
