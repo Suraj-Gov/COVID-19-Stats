6a1da0763f2fb4dc9ad5776476d8bc1869f16b2b
Suraj Govind <surajgovind332000@gmail.com>
2020-04-27 19:50:14 +0530

    Updates

---
+++ precache-manifest.3a4e3d92031b63635088dc98962382aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7338d348081caa631872223959901044",
+    "url": "/covid-19-stats/index.html"
+  },
+  {
+    "revision": "c91477455fbeababce23",
+    "url": "/covid-19-stats/static/css/main.761f2bac.chunk.css"
+  },
+  {
+    "revision": "ddcf262491d28066ce32",
+    "url": "/covid-19-stats/static/js/2.2aea42c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid-19-stats/static/js/2.2aea42c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c91477455fbeababce23",
+    "url": "/covid-19-stats/static/js/main.f64ad457.chunk.js"
+  },
+  {
+    "revision": "9d2c3011edf39237e2c1",
+    "url": "/covid-19-stats/static/js/runtime-main.7cea885c.js"
+  }
+]);
